8f60b4281844e7a13a6d38415f2d385bb6ee9f9f
Elena <elena0906@yandex.ru>
2023-04-04 16:54:06 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    StringBuilder sb = new StringBuilder();
-    long startTime = System.currentTimeMillis();
-    for (int i = 0; i < 200000; i++) {
-        sb.insert(0, "ABC");
+    public void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 200000; i++) {
+            sb.insert(0, "ABC");
+        }
+        System.out.println((System.currentTimeMillis() - startTime));
     }
-    System.out.println((System.currentTimeMillis() - startTime));
 }
 // /*   public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
